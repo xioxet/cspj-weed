a46332019fa8d187ed56ceeb37aff6fa0dad1174
e <conjunctivitis@protonmail.com>
2024-05-19 15:35:11 +0800

    em

--- api_cspj/middleware.py
+++ api_cspj/middleware.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseForbidden
 def restrict_access_middleware(get_response):
     def middleware(request):
         # Replace 'kong-proxy-ip' with the actual IP address of your Kong proxy
-        if request.META.get('HTTP_X_FORWARDED_FOR') != '192.168.226.129':
+        if request.META.get('HTTP_X_FORWARDED_HOST') != '192.168.226.129':
             return HttpResponseForbidden('Access Denied')
         return get_response(request)
     return middleware
