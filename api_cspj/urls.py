ce26948546d5c2ec25847636bf8ef30112b7cf22
root <qqqdyz@gmail.com>
2024-07-17 10:30:19 +0800

    main

--- api_cspj/urls.py
+++ api_cspj/urls.py
@@ -1,8 +1,10 @@
 # myapp/urls.py
 
 from django.urls import path
-from .views import MyModelListCreateAPIView
+from .views import MyModelListCreateAPIView, StoreListCreateAPIView, ProductListCreateAPIView
 
 urlpatterns = [
     path('mymodels/', MyModelListCreateAPIView.as_view(), name='mymodel-list-create'),
+    path('stores/', StoreListCreateAPIView.as_view(), name='store-list-create'),
+    path('products/', ProductListCreateAPIView.as_view(), name='product-list-create'),
 ]
